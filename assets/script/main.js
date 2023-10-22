5ce2ebda0c22420238e571c68ab12521e93f1fd2
yudistiraardi <mail.ardix@gmail.com>
2023-10-22 18:40:42 +0700

    Add method to render main news items

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,31 +1,52 @@
 import { GetNews } from './fetchData.js';
 import '../script/headlineNews.js';
+import '../script/mainNews.js'
 
 function main() {
   function gazaHeadline() {
     const gazaHeadline = new GetNews('https://newsapi.org/v2/everything?langauge=en&sortBy=popularity&searchin=title&pageSize=4&q=', 'gaza');
     gazaHeadline.fetchData()
-      .then(render)
+      .then(renderHeadline)
       .catch(responseMessage);
   }
 
   function homeHeadline() {
     const homeHeadline = new GetNews('https://newsapi.org/v2/top-headlines?pageSize=4&sources=', 'al-jazeera-english');
     homeHeadline.fetchData()
-      .then(render)
+      .then(renderHeadline)
       .catch(responseMessage);
   }
 
   function worldHeadline() {
     const worldHeadline = new GetNews('https://newsapi.org/v2/top-headlines?pageSize=4&country=', 'us');
     worldHeadline.fetchData()
-      .then(render)
+      .then(renderHeadline)
       .catch(responseMessage);
   }
 
-  function render(items) {
+  function gazaMainNews() {
+    const gazaMainNews = new GetNews('https://newsapi.org/v2/everything?langauge=en&searchin=title&pageSize=2&q=', 'gaza');
+    gazaMainNews.fetchData()
+     .then(renderMainNews)
+     .catch(responseMessage);
+  }
+
+  function homeMainNews() {
+    const homeMainNews = new GetNews('https://newsapi.org/v2/everything?langauge=en&searchin=title&pageSize=2&sources=', 'al-jazeera-english');
+    homeMainNews.fetchData()
+      .then(renderMainNews)
+      .catch(responseMessage);
+  }
+
+  function worldMainNews() {
+    const worldMainNews = new GetNews('https://newsapi.org/v2/everything?langauge=en&searchin=title&pageSize=2&country=', 'us');
+    worldMainNews.fetchData()
+      .then(renderMainNews)
+      .catch(responseMessage);
+  }
+
+  function renderHeadline(items) {
     items.forEach(item => {
-      console.log(item);
       const gazaHeadlineElement = document.createElement('headline-item');
       gazaHeadlineElement.setAttribute('class', 'grid-item');
       gazaHeadlineElement.newsItem = item;
@@ -36,13 +57,28 @@ function main() {
     })
   }
 
+  function renderMainNews(items) {
+    items.forEach(item => {
+      const gazaMainNewsElement = document.createElement('main-news');
+      gazaMainNewsElement.setAttribute('class', 'main-news');
+      gazaMainNewsElement.newsItem = item;
+
+      const mainContentElement = document.querySelector('.main-content');
+      mainContentElement.append(gazaMainNewsElement);
+    })
+  }
+
   function responseMessage(message) {
     alert(`Error: ${message}`);
   }
 
   // homeHeadline();
-  gazaHeadline();
-  // worldHeadline();
+  // gazaHeadline();
+  worldHeadline();
+
+  // gazaMainNews();
+  // homeMainNews();
+  worldMainNews()
 }
 
 export default main;
