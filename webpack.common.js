16d09a02d63840cebae07d92edf5b518de4d2553
yudistiraardi <mail.ardix@gmail.com>
2023-12-14 17:16:59 +0700

    Fix directory structure

--- webpack.common.js
+++ webpack.common.js
@@ -2,39 +2,39 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './assets/index.js',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module:{
+  module: {
     rules: [
       {
         test: /\.css$/,
         use: [
           {
-            loader: 'style-loader'
+            loader: 'style-loader',
           },
           {
-            loader: 'css-loader'
-          }
-        ]
+            loader: 'css-loader',
+          },
+        ],
       },
       {
         test: /\.html$/,
-        use: ['html-loader']
+        use: ['html-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html'
+      template: './assets/index.html',
+      filename: 'index.html',
     }),
   ],
   devServer: {
     port: 3000,
     hot: false,
     liveReload: true,
-  }
-}
+  },
+};
